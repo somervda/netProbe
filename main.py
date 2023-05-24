f7a65abc68079826754c2b5e1b4e35572b933594
David <39265971+somervda@users.noreply.github.com>
2023-05-23 21:44:29 -0400

    Added netconfig and connection logic

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-# main.py -- put your code here!
+# import os
+import time
+# import io
+# import sys
+
+import network
+import machine
+import ntptime
+
+from netconfig import NetConfig
+
+
+def connect():
+    netConfig = NetConfig()
+    if netConfig.useWiFi:
+        net_if = network.WLAN(network.STA_IF)
+    else:
+        # Connection details for a ESP32-Gateway board from Olimex (Ethernet connection)
+        net_if = network.LAN(mdc=machine.Pin(23), mdio=machine.Pin(18), power=machine.Pin(
+            12), phy_type=network.PHY_LAN8720, phy_addr=0)
+    net_if.active(True)
+    if not netConfig.dhcp:
+        # Use config settings for ifconfig if provided, otherwise will use dhcp
+        ifconfig = (netConfig.IP, netConfig.netmask,
+                    netConfig.gateway, netConfig.dns)
+        net_if.ifconfig(ifconfig)
+    if netConfig.useWiFi:
+        net_if.connect(netConfig.wifiSSID, netConfig.wifiPassword)
+    while not net_if.isconnected() or net_if.status() == 0 or net_if.ifconfig()[0] == "0.0.0.0":
+        print(".", end="")
+        time.sleep(.5)
+    print("ifconfig:", net_if.ifconfig())
+    ntptime.timeout = 2
+    if netConfig.ntp != "":
+        ntptime.host = netConfig.ntp
+    ntptime.settime()
+
+
+if __name__ == '__main__':
+    connect()
+    print("UMT time：%s" % str(time.localtime()))
---
+++ netconfig.py
@@ -0,0 +1,49 @@
+import json
+
+
+class NetConfig:
+    CONFIG_FILE = "netconfig.json"
+    useWiFi: bool
+    IP = ""
+    gateway = ""
+    dns = ""
+    netmask = ""
+    wifiSSID = ""
+    wifiPassword = ""
+    ntp = ""
+    dhcp: bool
+
+    def __init__(self):
+        self.getConfig()
+
+    def getConfig(self):
+        with open(self.CONFIG_FILE, "r") as configFile:
+            network = json.loads(configFile.read())
+            if "wifiSSID" in network:
+                self.wifiSSID = network["wifiSSID"]
+            if "wifiPassword" in network:
+                self.wifiPassword = network["wifiPassword"]
+            if "useWiFi" in network:
+                self.useWiFi = network["useWiFi"]
+                if self.wifiPassword == "" or self.wifiSSID == "":
+                    print("Missing wifiSSID or wifiPassword settings in netconfig.json")
+            else:
+                print("Missing useWifi setting in netconfig.json")
+            if "IP" in network:
+                self.IP = network["IP"]
+            if "gateway" in network:
+                self.gateway = network["gateway"]
+            if "dns" in network:
+                self.dns = network["dns"]
+            if "netmask" in network:
+                self.netmask = network["netmask"]
+            if "ntp" in network:
+                self.ntp = network["ntp"]
+            if "dhcp" in network:
+                self.dhcp = network["dhcp"]
+                if not self.dhcp:
+                    if self.IP == "" or self.gateway == "" or self.dns == "" or self.netmask == "":
+                        print(
+                            "Missing IP,gateway,dns,or netmask from netconfig.json for non dhcp connection")
+            else:
+                print("Missing dhcp setting in netconfig.json")
